b3adac56f6b3dd8abf0830ab7e17d0693a9fa449
ptdrpg <pagielamizael@gmail.com>
2026-03-21 07:41:53 +0300

    chore: add jwt dep

--- handler/depHandler.go
+++ handler/depHandler.go
@@ -7,7 +7,7 @@ import (
 )
 
 func AddAllDependancies(directory string, db string) {
-	var ressources = []string{"gorm.io/gorm", "github.com/go-chi/chi/v5", "github.com/joho/godotenv", filepath.Join("gorm.io/driver/", db), "github.com/spf13/cobra", "github.com/go-chi/cors", "github.com/golang-jwt/jwt"}
+	var ressources = []string{"gorm.io/gorm", "github.com/go-chi/chi/v5", "github.com/joho/godotenv", filepath.Join("gorm.io/driver/", db), "github.com/spf13/cobra", "github.com/go-chi/cors", "github.com/golang-jwt/jwt", "github.com/google/uuid", "golang.org/x/crypto/bcrypt", "github.com/golang-jwt/jwt/v5"}
 	for _, dep := range ressources {
 		installDep := exec.Command("go", "get", dep)
 		installDep.Dir = directory
